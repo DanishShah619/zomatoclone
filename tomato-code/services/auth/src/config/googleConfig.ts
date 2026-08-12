7eef4d393dd37084d1b0acf21167341e66bc9e3c
DanishShah619 <shanildanshah@gmail.com>
2026-08-12 22:22:28 +0530

    deployment issue4

--- tomato-code/services/auth/src/config/googleConfig.ts
+++ tomato-code/services/auth/src/config/googleConfig.ts
@@ -1,4 +1,4 @@
-import { google } from "googleapis";
+import { OAuth2Client } from "google-auth-library";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,7 +6,7 @@ dotenv.config();
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
-export const oauth2client = new google.auth.OAuth2(
+export const oauth2client = new OAuth2Client(
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
   "postmessage"
